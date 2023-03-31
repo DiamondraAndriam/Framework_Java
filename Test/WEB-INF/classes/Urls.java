8d3845a95a733e8162bc6946c71a088f79655d05
DiamondraAndriam <118435008+DiamondraAndriam@users.noreply.github.com>
2023-03-31 05:09:51 +0300

    framework to jar

--- Test/WEB-INF/classes/Urls.java
+++
@@ -1,9 +0,0 @@
-package etu1748.framework.annotation;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Urls {
-    String value();
-}
--- Test/WEB-INF/classes/compile.bat
+++
@@ -1,4 +0,0 @@
-javac --release 8 -d . Mapping.java
-javac --release 8 -d . Urls.java
-javac --release 8 -d . Util.java
-javac --release 8 -d . FrontServlet.java
